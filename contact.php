f0b11bd59d70509b7ae759255efb341ab027ba89
Amelie Aumont <maka.amelie@hotmail.com>
2018-10-04 11:40:24 +0200

    ajout de la page contact static

---
+++ contact.php
@@ -0,0 +1,75 @@
+
+<?php
+require "header.html";
+require 'pageStandard.php';
+?>
+
+<!-- Contact -->
+<section id="contact" class="formulaire">
+    <div class="container-fluid">
+        <div class="row justify-content-around">
+            <div class="col-lg-12">
+                <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input class="form-control" id="name" type="text" placeholder="Votre nom *" required="required" data-validation-required-message="Votre nom doit être inscrit.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" id="email" type="email" placeholder="Votre Email *" required="required" data-validation-required-message="Veuillez renseigner votre adresse mail.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group">
+                                <select class="form-control" id="Select" >
+                                    <option>Selectionnez un sujet...</option>
+                                    <option>Privatiser la JAVAPOP</option>
+                                    <option>Organiser un concert</option>
+                                    <option>Animation sur demande</option>
+                                    <option>Autre sujet</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" id="message" placeholder="Votre Message *" required="required" data-validation-required-message="S'il vous plait écrivez votre mesage."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="col-md-6 prestation">
+                            <ul class="list-unstyled">
+                                <li class="media">
+                                    <img class="mr-3" src="http://www.javapop.fr/ressources/img/event_location.jpg" height="150px" alt="Generic placeholder image">
+                                    <div class="media-body">
+                                        <h5 class="mt-0 mb-1">Privatiser La JAVAPOP</h5>
+                                    </div>
+                                </li>
+                                <li class="media my-4">
+                                    <div class="media-body">
+                                        <h5 class="mt-0 mb-1 text-right">Organiser un concert</h5>
+                                    </div>
+                                    <img class="mr-3" src="http://www.javapop.fr/ressources/img/event_concert.jpg" height="150px" alt="Generic placeholder image">
+                                </li>
+                                <li class="media">
+                                    <img class="mr-3" src="http://www.javapop.fr/ressources/img/event_insolite.jpg" height="150px" alt="Generic placeholder image">
+                                    <div class="media-body">
+                                        <h5 class="mt-0 mb-1">Animation sur demande</h5>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="clearfix"></div>
+                        <div class="col-lg-12 text-center">
+                            <div id="success"></div>
+                            <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php
+require "footer.html";
+?>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require "pageStandard.php";
           <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger mr-1 ml-1" style="width: 220px; height: 69px;" href="#cartes">Cartes</a>
           <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger mr-1 ml-1" style="width: 220px; height: 69px;" href="#galerie">Galerie</a>
           <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger mr-1 ml-1" style="width: 220px; height: 69px;" href="/eventPage.php">Evènements</a>
-          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger mr-1 ml-1" style="width: 220px; height: 69px;" href="#contact">Contact</a>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger mr-1 ml-1" style="width: 220px; height: 69px;" href="/contact.php">Contact</a>
           <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger mr-1 ml-1" style="width: 220px; height: 69px;" href="/OnEstLa.php">Nous trouver</a>
         </div>
     </header>
